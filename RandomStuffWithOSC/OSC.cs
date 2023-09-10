0609cebfee96c0ef19e54d865172350af9d27adc
DangerKiddy <42438297+DangerKiddy@users.noreply.github.com>
2023-09-11 04:21:52 +0700

    Добавьте файлы проекта.

---
+++ RandomStuffWithOSC/OSC.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace VRCSoundpad
+{
+    internal class OSC
+    {
+        private static CancellationTokenSource recvThreadCts;
+        private static Socket receiver;
+        public static void Init()
+        {
+
+            receiver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            receiver.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9001));
+        }
+
+        public static void Listen()
+        {
+            while (true)
+            {
+                recvThreadCts?.Cancel();
+
+                var newToken = new CancellationTokenSource();
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ct =>
+                {
+                    var token = (CancellationToken)ct;
+                    while (!token.IsCancellationRequested)
+                        Receive();
+                }), newToken.Token);
+
+                recvThreadCts = newToken;
+
+                Thread.Sleep(1);
+            }
+        }
+
+        private static byte[] buffer = new byte[2048 * 2];
+        private const string soundpadAvatarAddress = "/avatar/parameters/SNDP_";
+        private static void Receive()
+        {
+            try
+            {
+                var bytesReceived = receiver.Receive(buffer, buffer.Length, SocketFlags.None);
+
+                Msg msg = ParseOSC(buffer, bytesReceived);
+                if (msg.success && msg.address.StartsWith(soundpadAvatarAddress) && msg.value is bool && (bool)msg.value == true)
+                {
+                    Program.ReceiveSoundpadCommand(msg.address.Replace(soundpadAvatarAddress, ""));
+                }
+
+                Array.Clear(buffer, 0, bytesReceived);
+            }
+            catch (Exception e)
+            {
+                // Ignore as this is most likely a timeout exception
+                Console.WriteLine("Failed to receive message: {0}", e.Message);
+                return;
+            }
+        }
+
+        struct Msg
+        {
+            public string address;
+            public object value;
+            public bool success;
+        }
+        private static Msg ParseOSC(byte[] buffer, int length)
+        {
+            Msg msg = new Msg();
+            msg.success = false;
+
+            if (length < 4)
+                return msg;
+
+            int bufferPosition = 0;
+            string address = ParseString(buffer, length, ref bufferPosition);
+            if (address == "")
+                return msg;
+
+            msg.address = address;
+
+            // checking for ',' char
+            if (buffer[bufferPosition] != 44)
+                return msg;
+            bufferPosition++; // skipping ',' character
+
+            char valueType = (char)buffer[bufferPosition];
+            bufferPosition++;
+
+            object value = null;
+            switch (valueType)
+            {
+                case 'f':
+                    value = ParesFloat(buffer, length, bufferPosition);
+
+                    break;
+
+                case 'i':
+                    value = ParseInt(buffer, length, bufferPosition);
+
+                    break;
+
+                case 'F':
+                    value = false;
+
+                    break;
+
+                case 'T':
+                    value = true;
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            msg.value = value == null ? 0 : value;
+            msg.success = true;
+
+            return msg;
+        }
+
+        private static string ParseString(byte[] buffer, int length, ref int bufferPosition)
+        {
+            string address = "";
+
+            // first character must be '/'
+            if (buffer[0] != 47)
+                return address;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (buffer[i] == 0)
+                {
+                    bufferPosition = i + 1;
+
+                    if (bufferPosition % 4 != 0)
+                    {
+                        bufferPosition += 4 - (bufferPosition % 4);
+                    }
+
+                    break;
+                }
+
+                address += (char)buffer[i];
+            }
+
+            return address;
+        }
+
+        private static float ParesFloat(byte[] buffer, int length, int bufferPosition)
+        {
+            var valueBuffer = new byte[length - bufferPosition];
+
+            int j = 0;
+            for (int i = bufferPosition; i < length; i++)
+            {
+                valueBuffer[j] = buffer[i];
+
+                j++;
+            }
+
+            float value = bytesToFLoat(valueBuffer);
+            return value;
+        }
+
+        private static float bytesToFLoat(byte[] bytes)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes); // Convert big endian to little endian
+            }
+
+            float val = BitConverter.ToSingle(bytes, 0);
+            return val;
+        }
+
+        private static int ParseInt(byte[] buffer, int length, int bufferPosition)
+        {
+            var valueBuffer = new byte[length - bufferPosition];
+
+            int j = 0;
+            for (int i = bufferPosition; i < length; i++)
+            {
+                valueBuffer[j] = buffer[i];
+
+                j++;
+            }
+
+            int value = bytesToInt(valueBuffer);
+            return value;
+        }
+
+        private static int bytesToInt(byte[] bytes)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes); // Convert big endian to little endian
+            }
+
+            int val = BitConverter.ToInt32(bytes, 0);
+            return val;
+        }
+    }
+}
---
+++ RandomStuffWithOSC/Program.cs
@@ -0,0 +1,160 @@
+﻿using SharpOSC;
+using SoundpadConnector;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace VRCSoundpad
+{
+    internal class Program
+    {
+        private static Soundpad soundpad;
+
+        private static int totalCountOfSounds = 0;
+        private static Random random = new Random();
+        static void Main(string[] args)
+        {
+            InitSoundpad();
+            OSC.Init();
+            OSC.Listen();
+        }
+
+        private static void InitSoundpad()
+        {
+            soundpad = new Soundpad();
+            soundpad.ConnectAsync();
+            Thread.Sleep(1000);
+
+            totalCountOfSounds = (int)soundpad.GetSoundFileCount().Result.Value;
+        }
+
+        private static void SoundpadOnStatusChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(soundpad.ConnectionStatus);
+
+            if (soundpad.ConnectionStatus == ConnectionStatus.Connected)
+            {
+                soundpad.PlaySound(1);
+            }
+        }
+
+        public static Dictionary<int, bool> playedRandom = new(); 
+        public static void ReceiveSoundpadCommand(string command)
+        {
+            switch (command)
+            {
+
+                case "Random":
+                    soundpad.PlaySound(random.Next(1, totalCountOfSounds));
+
+                    break;
+
+                case "SmartRandom":
+                    if (playedRandom.Count >= totalCountOfSounds)
+                        playedRandom.Clear();
+
+                    int randId;
+                    do
+                    {
+                        randId = random.Next(1, totalCountOfSounds);
+                    }
+                    while (playedRandom.ContainsKey(randId));
+
+                    playedRandom[randId] = true;
+                    soundpad.PlaySound(randId);
+
+                    break;
+
+                case "Stop":
+                    soundpad.StopSound();
+
+                    break;
+
+                case "TogglePause":
+                    soundpad.TogglePause();
+
+                    break;
+
+                case "PlayLastPlayed":
+                    soundpad.PlayPreviouslyPlayedSound();
+
+                    break;
+
+                case "PlaySelectedSound":
+                    soundpad.PlaySelectedSound();
+
+                    break;
+
+                case "Forward1Sec":
+                    soundpad.Jump(1000);
+
+                    break;
+
+                case "Forward3Sec":
+                    soundpad.Jump(3000);
+                
+                    break;
+
+                default:
+                    try
+                    {
+                        soundpad.PlaySound(int.Parse(command));
+                    }
+                    catch { }
+
+                    break;
+            }
+            /*
+            if (command == "Random")
+                soundpad.PlaySound(random.Next(1, totalCountOfSounds));
+            else if (command == "SmartRandom")
+            {
+                if (playedRandom.Count >= totalCountOfSounds)
+                    playedRandom.Clear();
+
+                int randId;
+                do
+                {
+                    randId = random.Next(1, totalCountOfSounds);
+                }
+                while (playedRandom.ContainsKey(randId));
+
+                playedRandom[randId] = true;
+                soundpad.PlaySound(randId);
+            }
+            else if (command == "Stop")
+            {
+                soundpad.StopSound();
+            }
+            else if (command == "TogglePause")
+            {
+                soundpad.TogglePause();
+            }
+            else if (command == "PlayLastPlayed")
+            {
+                soundpad.PlayPreviouslyPlayedSound();
+            }
+            else if (command == "PlaySelectedSound")
+            {
+                soundpad.PlaySelectedSound();
+            }
+            else if (command == "Forward1Sec")
+            {
+                soundpad.Jump(1000);
+            }
+            else if (command == "Forward3Sec")
+            {
+                soundpad.Jump(3000);
+            }
+            else
+            {
+                try
+                {
+                    soundpad.PlaySound(int.Parse(command));
+                }
+                catch { }
+            }
+            */
+        }
+    }
+}
